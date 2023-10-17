46418ff8675cd6ff10cc7d9e1494af38fdeb2cc9
Chidi Tuke <c2kproductions@gmail.com>
2023-10-17 10:27:42 -0400

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 
 server.use(express.json());
 
-server.get('/hello', (req, res) => {
-  res.json('hello, there');
+const port = process.env.PORT || 9000;
+
+server.get("/hello", (req, res) => {
+  res.json("hello, there");
 });
 
-server.use('*', (req, res) => {
-  res.json({ message: 'API is UP!' })
+server.use("*", (req, res) => {
+  res.json({ message: "API is UP!" });
 });
 
-server.listen(9000, () => {
+server.listen(port, () => {
   console.log("\n*** Server Running on http://localhost:9000 ***\n");
 });
